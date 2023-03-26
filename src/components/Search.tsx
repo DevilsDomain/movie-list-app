d108cac176a608f67d128a9d7644dd2dac9420f6
DevilsDomain <alaraerzincan@gmail.com>
2023-03-26 10:28:11 +0200

    seach component renders, throws error when searching

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,21 +1,18 @@
 'use client';
-import { Movies } from './Movie';
 import { gql } from 'graphql-request';
 import { client } from '@/lib/client';
 import React, { useState } from 'react';
 
 interface SearchResults {
-    Poster: string;
-    Title: string;
-    Year: string;
-    Type: string;
-    imbdID: string;
-  }
-
-
+  Poster: string;
+  Title: string;
+  Year: string;
+  Type: string;
+  imdbID: string;
+}
 
 const GET_MOVIE_BY_TITLE = gql`
-query SearchMovieByTitle($title: String!) {
+  query SearchMovieByTitle($title: String!) {
     searchMovieByTitle(title: $title) {
       Poster
       Title
@@ -26,26 +23,29 @@ query SearchMovieByTitle($title: String!) {
   }
 `;
 
-async function Search() {
-    const [searchQuery, setSearchQuery] = useState('pirate');
-    const [searchResults, SetSearchResults] = useState<SearchResults[]>('');
-    const { searchMovieByTitle } = await client.request<{ searchMovieByTitle: SearchResults }>(GET_MOVIE_BY_TITLE, {
-        title: searchQuery,
-        });
-    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(event.target.value);
+function Search() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState<SearchResults[]>([]);
+  
+  const handleSearch = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { searchMovieByTitle } = await client.request<{ searchMovieByTitle: SearchResults[] }>(GET_MOVIE_BY_TITLE, {
+      title: event.target.value,
+    });
+    setSearchResults(searchMovieByTitle);
   };
-  console.log(searchMovieByTitle);
+  
   return (
     <div>
-         <input placeholder='Search Movie...' value={searchQuery} onChange={handleSearch} />
-         {searchMovieByTitle.map((movie, movieIndex) => {
-            return(
-                <p key={movieIndex}>{movie.Title}</p>
-            );
-         })}
+      <input placeholder='Search Movie...' value={searchQuery} onChange={handleSearch} />
+      {searchResults.length !== 0 ? (
+        searchResults.map((movie, movieIndex) => {
+          return <p key={movieIndex}>{movie.Title}</p>;
+        })
+      ) : (
+        <p>Search some movies!</p>
+      )}
     </div>
-  )
+  );
 }
 
-export default Search
+export default Search;
